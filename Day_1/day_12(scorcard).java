64cd437efc1c8ced6a645346a9f5fe1096b40976
Ashish Mishra <55228386+ashishmishra-dotcom@users.noreply.github.com>
2022-03-08 23:50:23 +0530

    Add files via upload

--- Day_1/day_12(scorcard).java
+++ Day_1/day_12(scorcard).java
@@ -1,49 +1,32 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
- public class Scorecard
-{
-private String teamname;
-private String innings;
-private int  score;
 
-public void setTeamname(String team)
-{
-this.teamname=team;
-}
+public class match{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the team name:\r");
+        String a = sc.nextLine();
+        System.out.println("Enter session:\r");
+        String b=sc.nextLine();
+        String c= " to win";
+        System.out.println("Enter runs:\r");
+        int z=sc.nextInt();
+        if(b.equals("First") || b.equals("first"))
+        {
+            ++z;
+            System.out.println("Name:" + a);
+            System.out.println("Scored:" + b);
+            System.out.println("Need " + z + c);
+        }
+       else if(b.equals("second") || b.equals("Second")){
+           System.out.println("Name:" + a);
+           System.out.println("Scored:" + b);
+           System.out.println("Matched Ended.");
+       }
+       else{
+        System.out.println("Invalid Input.");
+       }
+       sc.close();
+    }
 
-public String getTeamname(){
-return this.teamname;
-}
-public void setInnings(String in)
-{
-this.innings=in;
-}
-public String getInnings(){
-return this.innings;
-}
-public void setScore(int n)
-{
-this.score=n;
-}
-public int getScore()
-{
-return this.score;
-}
- 
-public void display(){
-
-if(innings.equals("First"))
-{
-System.out.println("Name: "+teamname);
-System.out.println("Scored: "+score);
-System.out.println("Need "+score+"+1 to win the match");
-}
-else 
-{
-System.out.println("Name: "+teamname);
-System.out.println("Scored: "+score);
-System.out.println("Matchended");
-}
-}
 }
